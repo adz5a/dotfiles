8a7e3481eea341598944e440bb0669e5daa1aa9a
antoine zimmermann <antoine.d.zimmermann@gmail.com>
2018-05-17 17:27:08 +0200

    Rework Makefile and install scripts

--- Makefile
+++ Makefile
@@ -1,15 +1,18 @@
 SESSION=dotfiles
 
 all:
-	git submodule update
+	git submodule update --init
 
 # need to manually add the bashrc in this
 # repo to the user's bashrc
 install:
-	./install.sh
+	./install.sh install
 
 uninstall:
-	./uninstall.sh
+	./install.sh uninstall
+
+upgrade:
+	./install.sh upgrade
 
 workspace:
 	tmux new-session -s $(SESSION) -n main
--- install.sh
+++ install.sh
@@ -1,6 +1,78 @@
-if [ -d $HOME/bin ]; then 
-    ln -s $PWD/diff-so-fancy/diff-so-fancy $HOME/bin/diff-so-fancy;
-else 
-    echo "No local bin present"
-    exit 1
-fi
+# Script to help installing this repo to create a new workstation easely. This
+# script must be called with an argument: install|uninstall.
+
+dotfiles='.gitconfig git_template .tmux.conf .ctags'
+startup_files='.bashrc .profile'
+
+install () {
+
+    if [ -d $HOME/bin ]; then
+        ln -s $PWD/diff-so-fancy/diff-so-fancy $HOME/bin/diff-so-fancy;
+    else
+        echo "No local bin present"
+        exit 1
+    fi
+
+    # for each of those dotfiles, create a symlink into the user's ~
+    for f in $dotfiles
+    do
+        ln -s $PWD/$f $HOME/$f
+    done
+
+    # for each of those dotfiles, append its content into the user's
+    # correspondig file. A control sequence could be used to enable easy removal
+    for f in $startup_files
+    do
+        cat $PWD/$f >> $HOME/$f
+    done
+
+}
+
+uninstall () {
+
+    if [ -d $HOME/bin/diff-so-fancy ]; then
+        rm -f $HOME/bin/diff-so-fancy
+    fi
+
+    # for each of those dotfiles remove file in the ~ folder.
+    # TODO: add a test to ensure you are not deleting something else
+    for f in $dotfiles
+    do
+        rm $HOME/$f
+    done
+
+}
+
+# will remove dotfiles found in the ~ and replace them with the current version
+# in the repository.
+upgrade () {
+
+    for f in $dotfiles
+    do
+        # if file exists, remove it
+        if [ -e $HOME/$f ]; then
+            rm $HOME/$f
+        fi
+        ln -s $PWD/$f $HOME/$f
+    done
+
+}
+
+case $1 in
+    install)
+        install
+        ;;
+    uninstall)
+        uninstall
+        ;;
+
+    upgrade)
+        upgrade
+        ;;
+    * )
+        echo "Commmand: install uninstall upgrade"
+        exit 1
+        ;;
+esac
+
+exit 0
