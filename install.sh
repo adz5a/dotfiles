e63b88343ed0f60e0a8243205f0a6a940743f8aa
antoine zimmermann <antoine.d.zimmermann@gmail.com>
2018-06-01 10:23:50 +0200

    add bash_aliases to install routine

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -eu
 # script must be called with an argument: install|uninstall.
 
 dotfiles='.gitconfig git_template .tmux.conf .ctags _vimrc'
-startup_files='.bashrc .profile'
+startup_files='.bashrc .profile .bash_aliases'
 
 install () {
 
